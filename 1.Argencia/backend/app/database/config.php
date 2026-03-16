f11bbf239ff075d5e08751a8322de27d16e7eed5
Rafael-Machado032 <rafaelmfds@gmail.com>
2026-03-16 20:58:26 -0300

    a

---
+++ 1.Argencia/backend/app/database/config.php
@@ -0,0 +1,6 @@
+php<?php
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3307'); // Sua porta personalizada
+define('DB_NAME', 'projeto_cms');
+define('DB_USER', 'root');
+define('DB_PASS', '');
