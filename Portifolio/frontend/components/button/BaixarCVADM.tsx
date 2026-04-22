c49fe08dc263ae3a8ebb58d0ce30f56a8022b36b
Rafael-Machado032 <rafaelmfds@gmail.com>
2026-04-22 19:24:09 -0300

    a

--- Portifolio/frontend/components/button/BaixarCVADM.tsx
+++ Portifolio/frontend/components/button/BaixarCVADM.tsx
@@ -4,7 +4,7 @@ import { useCurriculo } from "@/context/CurriculoContext";
 export default function BaixarCVADM() {
     const { curriculoDados } = useCurriculo();
     return (
-        <a href={curriculoDados?.curriculo_url_servidor} download className="flex gap-2 py-1 px-4 rounded-lg text-white font-bold bg-[#22c55e] ">
+        <a href={curriculoDados?.curriculo_url_servidor} download target="_blank" className="flex gap-2 py-1 px-4 rounded-lg text-white font-bold bg-[#22c55e] ">
             <Download size={20} />
             Baixar CV
         </a>
