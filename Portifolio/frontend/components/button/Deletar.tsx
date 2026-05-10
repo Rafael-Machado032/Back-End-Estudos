e46bb2b867116236072152c560334887236ab5c4
Rafael-Machado032 <rafaelmfds@gmail.com>
2026-05-10 18:21:47 -0300

    a

--- Portifolio/frontend/components/button/Deletar.tsx
+++ Portifolio/frontend/components/button/Deletar.tsx
@@ -3,36 +3,50 @@ import { DeletarFormacaoAction } from "@/api/FormacaoAPI"
 import { DeletarProjetoAction } from "@/api/ProjetoAPI"
 import { useFormacao } from "@/context/FormacaoContext"
 import { useProjeto } from "@/context/ProjetoContext"
+import { flushSync } from "react-dom"; // Ignora a regra do async
+import { useItem } from "@/context/IdEditar"; //Estado de item
 
 export default function Deletar({ id, tipo }: { id: string, tipo?: string }) {
-
+    const { setItemDados } = useItem();
     const { formacaoDados, setFormacaoDados } = useFormacao();
     const { projetoDados, setProjetoDados } = useProjeto();
 
-    const DeletarDados = async() => {
+    const DeletarDados = async () => {
+        flushSync(() => { // Solução para poder usar o carregamento
+            setItemDados({ carregando: true })
+        });
         let confirmMessage = null;
-        if (tipo === "formacao") {
-            if (confirm("Tem certeza que deseja deletar esta formação?")) {
-                confirmMessage = await DeletarFormacaoAction(id)
-                if (confirmMessage.success) {
-                    alert("Formação deletada com sucesso!")
-                    const listaEditada = formacaoDados.filter(p => p.id !== id);
-                    setFormacaoDados(listaEditada)
-                } else {
-                    alert("Ocorreu um erro ao deletar a formação.")
+        try {
+            if (tipo === "formacao") {
+                if (confirm("Tem certeza que deseja deletar esta formação?")) {
+                    confirmMessage = await DeletarFormacaoAction(id)
+                    if (confirmMessage.success) {
+                        alert("Formação deletada com sucesso!")
+                        const listaEditada = formacaoDados.filter(p => p.id !== id);
+                        setFormacaoDados(listaEditada)
+                    } else {
+                        alert("Ocorreu um erro ao deletar a formação.")
+                    }
+                }
+            } else {
+                if (confirm("Tem certeza que deseja deletar este projeto?")) {
+                    confirmMessage = await DeletarProjetoAction(id)
+                    if (confirmMessage.success) {
+                        alert("Projeto deletado com sucesso!")
+                        const listaEditada = projetoDados.filter(p => p.id !== id);
+                        setProjetoDados(listaEditada)
+                    } else {
+                        alert("Ocorreu um erro ao deletar o projeto.")
+                    }
                 }
             }
-        } else {
-            if (confirm("Tem certeza que deseja deletar este projeto?")) {
-                confirmMessage = await DeletarProjetoAction(id)
-                if (confirmMessage.success) {
-                    alert("Projeto deletado com sucesso!")
-                    const listaEditada = projetoDados.filter(p => p.id !== id);
-                    setProjetoDados(listaEditada)
-                } else {
-                    alert("Ocorreu um erro ao deletar o projeto.")
-                }
-            }
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+            alert("Ocorreu um erro inesperado.");
+        } finally {
+            // 2. Desativa o loading ao terminar (sucesso ou erro)
+            setItemDados({ carregando: false })
+            // console.log("Ultimo tipo: ", itemDados.tipo);
         }
     }
 
