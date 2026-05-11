974d487984620432724744aab65ef85c06310858
Rafael-Machado032 <rafaelmfds@gmail.com>
2026-05-11 18:32:46 -0300

    a

--- Portifolio/frontend/components/Portifolio/Projeto.tsx
+++ Portifolio/frontend/components/Portifolio/Projeto.tsx
@@ -12,7 +12,7 @@ export default function Projeto() {
 
                 {/* Container do Slider */}
                 <motion.div
-                    className="flex gap-6 cursor-grabbing active:cursor-grab "
+                    className="flex gap-6 cursor-grab"
                     drag="x" // Permite arrastar no eixo X
                     dragConstraints={{ right: 0, left: -600 }} // Ajuste o 'left' conforme o número de cards
                     whileTap={{ cursor: "grabbing" }}
