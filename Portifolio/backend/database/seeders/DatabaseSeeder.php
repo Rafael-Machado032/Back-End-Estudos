8dcbcf919b1f27c679e51722eeb07c4416694ee0
Rafael-Machado032 <rafaelmfds@gmail.com>
2026-05-18 21:30:12 -0300

    a

--- Portifolio/backend/database/seeders/DatabaseSeeder.php
+++ Portifolio/backend/database/seeders/DatabaseSeeder.php
@@ -3,23 +3,22 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Cria o usuário administrador se ele ainda não existir
+        User::firstOrCreate(
+            ['email' => 'admin@admin.com'],
+            [
+                'name' => 'Admin',
+                'password' => bcrypt('goby0321'),
+            ]
+        );
     }
 }
